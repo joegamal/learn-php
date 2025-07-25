086323cf0c64765dbe839808c2f0cadd44f42797
joegamal <yusufmagamalth@gmail.com>
2025-07-25 20:34:47 +0300

    require and include

---
+++ require_include.php
@@ -0,0 +1,14 @@
+<?php 
+
+/**
+ * what is the difference between the require and include functions 
+ * 
+ * the include include function doesnot stop every thing if it didnot find the file
+ * 
+ * 
+ * the require function stops everything if it didnot find the function
+ */
+
+
+
+include("some_file.some_extenstion");
